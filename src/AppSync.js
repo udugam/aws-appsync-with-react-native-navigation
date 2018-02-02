ebc747191a280a2c418b679c3422e89eca2366fc
udugam <ryan.udugam@gmail.com>
2018-02-02 00:20:39 -0500

    finished appSync console setup and installed dependencies

---
+++ src/AppSync.js
@@ -0,0 +1,6 @@
+﻿export default {
+    "graphqlEndpoint": "https://rztrtynp2jbxfouoneevp5uzkq.appsync-api.us-east-1.amazonaws.com/graphql",
+    "region": "us-east-1",
+    "authenticationType": "API_KEY",
+    "apiKey": "da1-bGoaka67RNmU3WVX8cML0Q"
+}
