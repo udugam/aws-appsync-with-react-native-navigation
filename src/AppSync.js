fbb44a2974f7a815473c236ca46d6ec296a250dc
udugam <ryan.udugam@gmail.com>
2018-02-15 02:30:43 -0500

    Revised as per AppSyncUpdate

--- src/AppSync.js
+++ src/AppSync.js
@@ -1,6 +1,6 @@
 ﻿export default {
-    "graphqlEndpoint": "https://vzyx5ae2urahfibxzkr6tsdh24.appsync-api.us-east-1.amazonaws.com/graphql",
+    "graphqlEndpoint": "https://g7um2kkq6bdurn3aeyyem5ctaq.appsync-api.us-east-1.amazonaws.com/graphql",
     "region": "us-east-1",
     "authenticationType": "API_KEY",
-    "apiKey": "da1-dnDI-WHZTvam1R27hlKTQw"
+    "apiKey": "da2-nvnsqcn3sng2zceklvlpiyw4mu"
 }
